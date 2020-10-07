c425fcbc0398da657c2403111b32f040387cf17b
takkyu <mk.st.phys@gmail.com>
2020-10-07 10:24:22 +0900

    Parser complete

--- parser.cpp
+++ parser.cpp
@@ -1,5 +1,7 @@
 #include "regex.h"
 
+extern Token *token;
+
 Token* parser(std::string input) {
     Token* head = new Token('A');
     Token* now = head;
@@ -14,8 +16,71 @@ Token* parser(std::string input) {
         now->next = newtoken;
         now = now->next;
     }
+    Token *newtoken = new Token('A');
+    newtoken->kind = Tokenkind::EOS;
+    now->next = newtoken;
+
     Token* ans = head->next;
     delete head;
     return ans;
 }
 
+bool at_eos() {
+    return token->kind == Tokenkind::EOS;
+}
+
+bool consume(char op) {
+    if (token->kind == Tokenkind::SPECIAL && token->chr == op) {
+        token = token->next;
+        return true;
+    }
+    return false;
+}
+
+void expect(char op) {
+    if (token->chr != op) {
+        std::cerr << "Expect " << op << ", but we have " << token->chr << "\n";
+        exit(1);
+    }
+    token = token->next;
+}
+
+char expect_chr() {
+    if (token->kind != Tokenkind::TERMINAL) {
+        std::cerr << "Expect number, but we have " << token->chr << "\n";
+        exit(1);
+    }
+    char chr = token->chr;
+    token = token->next;
+    return chr;
+}
+
+Node* expr() {
+    if (at_eos()) return nullptr;
+    Node* node = primes();
+    return new Node('.', node, expr(), Nodekind::CONCAT);
+}
+
+Node* primes() {
+    Node* node = primary();
+    if (consume('?')) {
+        return new Node('?', node, nullptr, Nodekind::QUESTION);
+    } else if (consume('*')) {
+        return new Node('*', node, nullptr, Nodekind::STAR);
+    } else if (consume('+')) {
+        return new Node('+', node, nullptr, Nodekind::PLUS);
+    } else if (consume('|')) {
+        return new Node('|', node, primary(), Nodekind::OR);
+    } else {
+        return node;
+    }
+}
+
+Node* primary() {
+    if (consume('(')) {
+        Node* node = expr();
+        expect(')');
+        return node;
+    }
+    return new Node(expect_chr(), nullptr, nullptr, Nodekind::CHR);
+}
--- regex.h
+++ regex.h
@@ -7,14 +7,39 @@ const int ALPHA_NUM = 26*2;
 enum class Tokenkind {
     TERMINAL,
     SPECIAL,
+    EOS,
+};
+
+enum class Nodekind {
+    CONCAT,
+    OR,
+    QUESTION,
+    STAR,
+    PLUS,
+    CHR,
 };
 
 struct Token {
     Tokenkind kind;
     char chr;
     Token* next;
-
     Token(char input_chr) : chr {input_chr}, kind {Tokenkind::TERMINAL}, next {nullptr} {};
 };
 
+struct Node {
+    Nodekind kind;
+    char chr;
+    Node* lhs;
+    Node* rhs;
+    Node(char input_chr, Node* lhs, Node* rhs, Nodekind kind) : chr {input_chr}, lhs {lhs}, rhs {rhs}, kind {kind} {};
+};
+
 Token* parser(std::string input);
+
+Node* expr();
+Node* primes();
+Node* primary();
+char expect_chr();
+void expect(char op);
+bool at_eos();
+bool consume(char op);
