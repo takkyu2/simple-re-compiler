b07b33a93b442d5430b37152b37f44c370984c42
takkyu <mk.st.phys@gmail.com>
2020-10-08 16:15:39 +0900

    Added dots

--- parser.cpp
+++ parser.cpp
@@ -2,12 +2,33 @@
 
 extern Token *token;
 
+std::string add_dots(std::string input) {
+    std::string added_str;
+    if (input.size() == 0) {
+        std::cerr << "ERROR, empty string\n";
+        exit(1);
+    }
+    added_str.push_back(input[0]);
+    for (int i = 1; i < input.size(); ++i) {
+        char fr = input[i-1], ba = input[i];
+        if (fr == '|' || ba == '|' || fr == '(' || ba == ')' || ba == '?' || ba == '*' || ba == '+') {
+            added_str.push_back(ba);
+            continue;
+        }
+        added_str.push_back('.');
+        added_str.push_back(ba);
+    }
+    return added_str;
+}
+
 Token* parser(std::string input) {
+    input = add_dots(input);
+    std::cout << input << "\n";
     Token* head = new Token('A');
     Token* now = head;
     for (char chr : input) {
         Token *newtoken = new Token(chr);
-        if (chr == '(' || chr == ')' || chr == '?' || chr == '|' || chr == '+' || chr == '*') {
+        if (chr == '(' || chr == ')' || chr == '?' || chr == '|' || chr == '+' || chr == '*' || chr == '.') {
             newtoken->kind = Tokenkind::SPECIAL;
         } else if (!isalpha(chr)) {
             std::cerr << "ERROR:INVALID SYMBOL\n";
@@ -47,9 +68,8 @@ void expect(char op) {
 
 char expect_chr() {
     if (token->kind != Tokenkind::TERMINAL) {
-        return '%';
-        /* std::cerr << "Expect char, but we have " << token->chr << "\n"; */
-        /* exit(1); */
+        std::cerr << "Expect char, but we have " << token->chr << "\n";
+        exit(1);
     }
     char chr = token->chr;
     token = token->next;
@@ -57,9 +77,16 @@ char expect_chr() {
 }
 
 Node* expr() {
-    if (at_eos()) return new Node('E', nullptr, nullptr, Nodekind::EOS);
     Node* node = primes();
-    return new Node('.', node, expr(), Nodekind::CONCAT);
+    if (consume('.')) 
+        return new Node('.', node, expr(), Nodekind::CONCAT);
+    else {
+        if (at_eos()) {
+            Node* rhs =  new Node('E', nullptr, nullptr, Nodekind::EOS);
+            return new Node('.', node, rhs, Nodekind::CONCAT);
+        } else 
+            return node;
+    }
 }
 
 Node* primes() {
@@ -84,6 +111,6 @@ Node* primary() {
         return node;
     }
     char chr = expect_chr();
-    if (chr == '%') return nullptr;
+    /* std::cout << chr << "\n"; */
     return new Node(chr, nullptr, nullptr, Nodekind::CHR);
 }
--- test.sh
+++ test.sh
@@ -14,7 +14,21 @@ assert() {
     fi
 }
 
-assert 1 "abc abc"
+assert 1 "a a"
 assert 0 "abc def"
+assert 1 "abc abc"
 assert 1 "abc (abc)"
-assert 1 "abcabcabc (abc)*"
+
+assert 0 "a a(abc)+"
+assert 1 "aabc a(abc)+"
+assert 1 "abcabcabc (abc)+"
+
+assert 0 "abcabc (abc)?"
+assert 1 "abc (abc)?"
+assert 1 "a a(abc)?"
+
+
+# assert 1 "abc (abc)*"
+# assert 1 "abcabc (abc)*"
+# assert 1 "a a(abc)*"
+
